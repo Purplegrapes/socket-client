e36a7d878dedf100abb5c9bb3dfcf4c18994cdac
琼 <apple@qiong.local>
2023-09-19 16:22:48 +0800

    fix lint

---
+++ .eslintrc.js
@@ -0,0 +1,77 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:lodash-fp/recommended'
+  ],
+  plugins: ['import', '@typescript-eslint', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'lf', jsxSingleQuote: false }],
+    'import/extensions': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    'comma-dangle': 0,
+    'react/function-component-definition': 0,
+    'object-curly-newline': 0,
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'jsx-quotes': [2, 'prefer-double'],
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/triple-slash-reference': 0,
+    'max-len': 0,
+    '@typescript-eslint/no-empty-interface': 1,
+    'no-param-reassign': 0,
+    'react/button-has-type': 0,
+    'import/prefer-default-export': 0,
+    'no-promise-executor-return': 0,
+    'operator-linebreak': 0,
+    'implicit-arrow-linebreak': 0,
+    indent: [0, 0],
+    'import/no-cycle': 0,
+    'import/order': [
+      2,
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type']
+      }
+    ],
+    'react/prop-types': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'react/display-name': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/require-default-props': 0,
+    'react/jsx-props-no-spreading': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'warn',
+    'jsx-a11y/alt-text': 0
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.ts', '.tsx']
+      }
+    },
+    react: {
+      version: 'detect'
+    }
+  }
+};
--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, { useState, useCallback } from 'react';
 import { Space, Button, Progress, message, Alert } from 'antd';
-import socketIO from "socket.io-client";
+import socketIO from 'socket.io-client';
 import JSONInput from 'react-json-editor-ajrm';
-import { v4 } from 'uuid'
-import locale from "react-json-editor-ajrm/locale/en";
+import { v4 } from 'uuid';
+import locale from 'react-json-editor-ajrm/locale/en';
 
 let socket = null;
 
 const ERROR = 'ERROR';
 const PROGRESS = 'PROGRESS_TASK';
 
-const defaultUrl =  "http://api4.betalpha.com:81/socket";
-const defaultToken = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1MDgzNTQ0NC0xZjY5LTRmNTEtYmZlMy02ZGQyNDJiZDdmZTgiLCJleHAiOjM2ODk4NDE4MTksImF1dGhvcml0aWVzIjpbIlVTRVIiXSwianRpIjoiNWcxOHM5aVJtbFIyNDRXSE1sY2VoSmdESG0wIiwiY2xpZW50X2lkIjoib2ZmaWNpYWxfd2Vic2l0ZSIsInNjb3BlIjpbInJpc2tTeXN0ZW0iXX0.iLNVOa83FYsIywZ0mGEcxS-77X1lw_fN5O3onzNi_eRZ9RXN9MU1H9zWJvD-8TeYhOGVRCbxmEhNyYwxxKFRxdantl0gCyy70ZYLUOoJI3cqYA0dR3cNMgLxVIvAH1d51kb35W8P-AN9BNX-UmBxjlNBHB7Ladd_BvDVnGdBXq3sAY4IHaEKdf0T3kR9gVU2wsGmppN6PJlDsPqGkKTfuapjVS1odhNMydald3SAbjmWCjW6M36OdkMPAiFI9E69LvkYs6ZacBLKW_wVtkPHrZuuTEPoHs5hAUOi45Fp32U9V6RroNKQqUUU3GScRqBGKONhj2Kecm5p3fbqGrLtrA'
+const defaultUrl = 'http://api4.betalpha.com:81/socket';
+const defaultToken =
+  'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1MDgzNTQ0NC0xZjY5LTRmNTEtYmZlMy02ZGQyNDJiZDdmZTgiLCJleHAiOjM2ODk4NDE4MTksImF1dGhvcml0aWVzIjpbIlVTRVIiXSwianRpIjoiNWcxOHM5aVJtbFIyNDRXSE1sY2VoSmdESG0wIiwiY2xpZW50X2lkIjoib2ZmaWNpYWxfd2Vic2l0ZSIsInNjb3BlIjpbInJpc2tTeXN0ZW0iXX0.iLNVOa83FYsIywZ0mGEcxS-77X1lw_fN5O3onzNi_eRZ9RXN9MU1H9zWJvD-8TeYhOGVRCbxmEhNyYwxxKFRxdantl0gCyy70ZYLUOoJI3cqYA0dR3cNMgLxVIvAH1d51kb35W8P-AN9BNX-UmBxjlNBHB7Ladd_BvDVnGdBXq3sAY4IHaEKdf0T3kR9gVU2wsGmppN6PJlDsPqGkKTfuapjVS1odhNMydald3SAbjmWCjW6M36OdkMPAiFI9E69LvkYs6ZacBLKW_wVtkPHrZuuTEPoHs5hAUOi45Fp32U9V6RroNKQqUUU3GScRqBGKONhj2Kecm5p3fbqGrLtrA';
 
 const App = () => {
-
   const [meta, setMeta] = useState({
     startEventName: 'START_FUND_DETAIL_TAG_FILTER',
     finishEventName: 'FINISH_FUND_DETAIL_TAG_FILTER',
     taskMeta: {
       meta: {},
       task: {
-        dataSourceId: "baef693e-bbee-4bb2-a4f5-66bc538d3c66",
-        locale: "zh",
-        clientVersion: "webapp",
-        taskId: v4(),
+        dataSourceId: 'baef693e-bbee-4bb2-a4f5-66bc538d3c66',
+        locale: 'zh',
+        clientVersion: 'webapp',
+        taskId: v4()
       },
-      authorization: defaultToken,
-    },
+      authorization: defaultToken
+    }
   });
   const [authorizationMeta, setAuthorizationMeta] = useState({
     url: defaultUrl,
@@ -35,74 +35,71 @@ const App = () => {
     socketPath: '/socket',
     taskMeta: {
       task: {
-        dataSourceId: "baef693e-bbee-4bb2-a4f5-66bc538d3c66",
-        locale: "zh",
-        clientVersion: "webapp",
-        taskId: v4(),
+        dataSourceId: 'baef693e-bbee-4bb2-a4f5-66bc538d3c66',
+        locale: 'zh',
+        clientVersion: 'webapp',
+        taskId: v4()
       },
-      authorization: defaultToken,
-    },
-
+      authorization: defaultToken
+    }
   });
   const [result, setResult] = useState({});
-  const connect = useCallback (() => {
+  const connect = useCallback(() => {
     if (authorizationMeta) {
-      socket = socketIO(authorizationMeta.url,{
-        transports: ["polling", "websocket"],
+      socket = socketIO(authorizationMeta.url, {
+        transports: ['polling', 'websocket'],
         path: authorizationMeta.socketPath
       });
       socket.connect(() => {
-        console.log(socket.id)
-      })
+        console.log(socket.id);
+      });
       socket.on(ERROR, (response) => {
-        console.log(response)
+        console.log(response);
         setResult({
           progress: -1,
           message: response?.fieldError.message
-        })
-      })
-      socket.on("connect_error", (error) => {
+        });
+      });
+      socket.on('connect_error', (error) => {
         console.log(error); // true
       });
       socket.emit(authorizationMeta.startEventName, authorizationMeta.taskMeta, (response) => {
         console.log(response.status); // ok
-        message.success('鉴权成功')
+        message.success('鉴权成功');
       });
     } else {
-      message.warning('请输入鉴权信息')
+      message.warning('请输入鉴权信息');
     }
   }, [authorizationMeta]);
 
   const onFinish = useCallback(() => {
     if (socket && socket.connected) {
       setResult({});
-      socket.emit(meta.startEventName, (meta.taskMeta), (response) => {
+      socket.emit(meta.startEventName, meta.taskMeta, (response) => {
         console.log(response.status); // ok
-      })
+      });
       socket.on(meta?.finishEventName, (response) => {
-        console.log(response)
+        console.log(response);
         setResult({
           progress: 1,
           data: response
-        })
-      })
+        });
+      });
       socket.on(PROGRESS, (response) => {
-        console.log(response)
+        console.log(response);
         setResult({
-          progress: response.progress,
-        })
-      })
+          progress: response.progress
+        });
+      });
     } else {
-      message.info("请先连接socket")
+      message.info('请先连接socket');
     }
   }, [meta]);
   return (
-    <div style={{ padding: "10px 20px" }}>
-    <h5>
-          SOCKET--CLIENT
-        </h5>
-        authorizationMeta:
-        <JSONInput
+    <div style={{ padding: '10px 20px' }}>
+      <h5>SOCKET--CLIENT</h5>
+      authorizationMeta:
+      <JSONInput
         id="authorization"
         locale={locale}
         placeholder={authorizationMeta}
@@ -111,7 +108,7 @@ const App = () => {
             console.log('ERROR: ', obj.error);
           } else {
             console.log(obj.plainText);
-            setAuthorizationMeta(obj.jsObject)
+            setAuthorizationMeta(obj.jsObject);
           }
         }}
         colors={{
@@ -120,7 +117,7 @@ const App = () => {
           string: '#ce8453',
           number: '#ce8453',
           keys: '#386fa4',
-          background_warning: '#ffffff',
+          background_warning: '#ffffff'
         }}
         style={{
           outerBox: { border: '10px solid green' },
@@ -133,16 +130,14 @@ const App = () => {
             console.log(obj.plainText);
           }
         }}
-       height="250px"
-       width='800px'
+        height="250px"
+        width="800px"
       />
       <Button type="primary" onClick={connect} style={{ margin: 20 }}>
-          连接
+        连接
       </Button>
-        <div>
-        taskMeta:
-       </div>
-        <JSONInput
+      <div>taskMeta:</div>
+      <JSONInput
         id="a_unique_id"
         locale={locale}
         placeholder={meta}
@@ -151,7 +146,7 @@ const App = () => {
             console.log('ERROR: ', obj.error);
           } else {
             console.log(obj.plainText);
-            setMeta(obj.jsObject)
+            setMeta(obj.jsObject);
           }
         }}
         colors={{
@@ -160,10 +155,10 @@ const App = () => {
           string: '#ce8453',
           number: '#ce8453',
           keys: '#386fa4',
-          background_warning: '#ffffff',
+          background_warning: '#ffffff'
         }}
         style={{
-          outerBox: { border: '10px solid green' },
+          outerBox: { border: '10px solid green' }
         }}
         onChange={(obj) => {
           if (obj.error) {
@@ -172,8 +167,8 @@ const App = () => {
             console.log(obj.plainText);
           }
         }}
-       height="250px"
-       width='800px'
+        height="250px"
+        width="800px"
       />
       <Space style={{ margin: 20 }}>
         <Button type="primary" onClick={onFinish}>
@@ -182,32 +177,24 @@ const App = () => {
 
         <Button onClick={() => setMeta({})}>清除meta</Button>
       </Space>
-    {result?.progress === -1 && (
-       <Alert
-       message="Error"
-       description={result.message}
-       type="error"
-       showIcon
-     />
-    )}
-
-    {result?.progress  && result?.progress !== 1 && <Progress percent={result?.progress * 100} />}
+      {result?.progress === -1 && <Alert message="Error" description={result.message} type="error" showIcon />}
+      {result?.progress && result?.progress !== 1 && <Progress percent={result?.progress * 100} />}
       {result?.progress === 1 && (
         <JSONInput
-        id="output"
-        confirmGood={false}
-        placeholder={result?.data} // data to display
-        theme="light_mitsuketa_tribute"
-        locale={locale}
-        viewOnly
-        colors={{
-          string: "green" // overrides theme colors with whatever color value you want
-        }}
-        height="550px"
-        width='800px'
-       />
+          id="output"
+          confirmGood={false}
+          placeholder={result?.data} // data to display
+          theme="light_mitsuketa_tribute"
+          locale={locale}
+          viewOnly
+          colors={{
+            string: 'green' // overrides theme colors with whatever color value you want
+          }}
+          height="550px"
+          width="800px"
+        />
       )}
     </div>
-  )
+  );
 };
 export default App;
